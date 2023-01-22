1670c851430eec0d57c4f939cbde9d3cec6ac148
Tom-Trav <122391169+Tom-Trav@users.noreply.github.com>
2023-01-22 15:46:40 +0000

    added some basic render logic

--- components/home.js
+++ components/home.js
@@ -3,7 +3,6 @@ const cities = document.getElementById("cities-btn")
 const cars = document.getElementById("car-btn")
 const fashion = document.getElementById("fashion-btn")
 const alphabet = document.getElementById("alphabet")
-const ulEl = documnent.getele("ul-el")
 
 const chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-"]
 
@@ -14,15 +13,31 @@ let catMsg = "You chose the category: "
 
 cities.addEventListener("click", function() {
     choice.textContent = catMsg + "'CITIES'"
+    game()
 })
 cars.addEventListener("click", function() {
     choice.textContent = catMsg + "'CAR BRANDS'"
+    game()
 })
 fashion.addEventListener("click", function() {
     choice.textContent = catMsg + "'FASHION BRANDS'"
+    game()
 })
 
 
+
+function game() {
+    let list = ""
+    for (let i = 0; i < chars.length; i++) { 
+        list += `
+        <li>
+        <button>
+            ${chars[i]}
+        </button>
+        </li>`
+    }
+    alphabet.innerHTML = list
+}
 
 
 
